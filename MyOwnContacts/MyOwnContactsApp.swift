64282712dbaad2a2d75ccaca720cdab5f84e97cd
Avinash Reddy <avinashraamireddy@gmail.com>
2022-09-15 13:32:07 +0530

    Initial commit

---
+++ MyOwnContacts/MyOwnContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOwnContactsApp.swift
+//  MyOwnContacts
+//
+//  Created by Avinash Reddy on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MyOwnContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
