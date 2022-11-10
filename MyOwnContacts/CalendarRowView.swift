93dfc86d660678b781adc42b471f763cbc585eed
Avinash Reddy <avinashraamireddy@gmail.com>
2022-11-11 00:03:20 +0530

    ui refactored

--- MyOwnContacts/CalendarRowView.swift
+++ MyOwnContacts/CalendarRowView.swift
@@ -12,51 +12,32 @@ struct CalendarRowView: View {
     
     var body: some View {
         let containerWidth:CGFloat = UIScreen.main.bounds.width
-            HStack(alignment: .top, spacing: 0) {
-                VStack() {
-                    Text(entry.heading)
-                    Text(entry.timezone)
-                }
-                .frame(width:containerWidth * 0.15)
-//                .background(.green)
-                
-                
-                VStack() {
-                    Text(entry.body)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-//                .background(.yellow)
-                    
+        let width: CGFloat = 10
+        HStack(alignment: .top, spacing: 0) {
+            VStack() {
+                Text(entry.heading)
+                Text(entry.timezone)
+            }
+            .frame(width: containerWidth * 0.15)
             
+            
+            VStack(alignment: .center, spacing: 0) {
+                Circle()
+                    .frame(width: width, height: width)
+                    .foregroundColor(.green)
+                Rectangle()
+                    .frame(width: width/4)
+                    .foregroundColor(.green)
+            }
+            .frame(width: width*1.5)
+            .background(.clear)
+            
+            
+            VStack() {
+                Text(entry.body)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
         }
-        
     }
-    
-//    var body: some View {
-//        let containerWidth:CGFloat = UIScreen.main.bounds.width
-//        let fixedWidth: CGFloat = 10
-//        HStack(alignment: .top) {
-//            //Date & Time
-//            VStack(alignment: .center) {
-//                Text(entry.heading).foregroundColor(.red)
-//                Text(entry.timezone).foregroundColor(.yellow)
-//            }.frame(width: containerWidth * 0.1)
-//                .background(.orange)
-//
-//            //Separator
-//            VStack(alignment: .center,spacing: 0) {
-//                Circle().frame(width: fixedWidth,height: fixedWidth).foregroundColor(.purple)
-//                Rectangle().frame(width:fixedWidth/4)
-//            }.frame(width: fixedWidth)
-//                .background(.purple)
-//
-//
-//            //Description
-//            VStack(alignment: .leading) {
-//                Text(entry.body).foregroundColor(.green)
-//            }.frame(width: (containerWidth * 0.8))
-//            .background(.red)
-//        }.background(.green)
-//    }
 }
- 
+
--- MyOwnContacts/CalendarView.swift
+++ MyOwnContacts/CalendarView.swift
@@ -11,15 +11,15 @@ struct CalendarView: View {
     @State private var entries = CalendarServices.shared.getAllEntries()
     
     var body: some View {
-        NavigationStack{ 
+        NavigationStack {
             List(entries) { entry in
                 NavigationLink(value: entry) {
                     CalendarRowView(entry: entry)
                 }
-            } 
+            }
             .navigationDestination(for: CalendarEntry.self, destination: { entry in
                 Text(entry.body)
             })
         }
     }
-} 
+}
